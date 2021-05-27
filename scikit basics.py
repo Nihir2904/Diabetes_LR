fa56d3b669ec2a133a9b34612134eb5c2f861e78
Nihir2904 <74675767+Nihir2904@users.noreply.github.com>
2021-05-28 03:35:29 +0530

    Update and rename scikit basics.py to diabetes_ml.py

--- scikit basics.py
+++
@@ -1,7 +0,0 @@
-import sklearn
-from sklearn import tree
-features = [[2, 100], [3,50], [5,200], [4, 500], [7, 800]]
-label = [1,2,1,2,2]
-cld = tree.DecisionTreeClassifier()
-cld = cld.fit(features, label)
-print(cld.predict([[3, 50]]))
